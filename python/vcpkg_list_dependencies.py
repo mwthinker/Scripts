79afd43c868b6840578dfcf61ae5052dfef3d60e
Marcus Welander <mwthinker@yahoo.se>
2024-04-24 22:28:44 +0200

    Use argparse and make filepath argument either be vcpkg.json or folder containing it

--- python/vcpkg_list_dependencies.py
+++ python/vcpkg_list_dependencies.py
@@ -2,17 +2,23 @@
 import os
 import platform
 import subprocess
-import sys
 import json
+import argparse
 
-def get_vcpkg_executable():
+def get_vcpkg_executable() -> str:
   if platform == "win32":
     return "vcpkg.exe"
   else:
     return "vcpkg"
 
-def vcpkg_list_deps(vcpkg_json_path):
-  path = os.path.join(vcpkg_json_path, 'vcpkg.json')
+def vcpkg_list_deps(vcpkg_json_path: str, verbose: bool):
+  if os.path.isdir(vcpkg_json_path):
+    path = os.path.join(vcpkg_json_path, 'vcpkg.json')
+  elif vcpkg_json_path.endswith('vcpkg.json'):
+    path = vcpkg_json_path
+  else:
+    print(f"Error: Invalid path {vcpkg_json_path}")
+    return
 
   # Check if the vcpkg.json file exists
   if not os.path.isfile(path):
@@ -35,23 +41,21 @@ def vcpkg_list_deps(vcpkg_json_path):
   
   # Run the bash command
   for dep in data['dependencies']:
-    bash_command = f"{vcpkg} depend-info {dep} --format=tree"
-    #print(f"Running command: {bash_command}")
     bash_command = f"{vcpkg} depend-info {dep} --format=tree 2>&1"
+    if verbose:
+      print(f"Running command: {bash_command}")
     subprocess.run(bash_command, shell=True)
 
-def print_usage():
-  print("Usage: python vcpkg_list_dependencies.py <path-to-vcpkg.json>")
-  print("List all dependencies in the vcpkg.json (uses 'vcpkg depend-info' command)")
-  print()
+def run(args):
+  vcpkg_list_deps(args.vcpkg_json_path, args.verbose)
 
 def main():
-  if len(sys.argv) < 2:
-    print_usage()
-    return
+  parser = argparse.ArgumentParser(description = "List all dependencies in the vcpkg.json (uses 'vcpkg depend-info' command)")
+  parser.add_argument("vcpkg_json_path", help="path to the vcpkg.json file or folder containing the vcpkg.json file")
+  parser.add_argument("-v", "--verbose", help="print verbose output", dest="verbose", action="store_const", const=True)
+  parser.set_defaults(func=run)
+  args=parser.parse_args()
+  args.func(args)
 
-  vcpkg_json_path = sys.argv[1]
-  vcpkg_list_deps(vcpkg_json_path)
-
-if __name__=="__main__": 
-    main() 
+if __name__=="__main__":
+  main()
